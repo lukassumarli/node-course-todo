0e2edb54a30afe5c74abc288acd5433339bb0d5c
Lukas Sumarli <lukassumarli@gmail.com>
2017-10-14 09:40:40 +0000

    add post todos

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+    .then((result) => { res.send(result) })
+    .catch((err) => { res.status(400).send(err) });
+});
+
+app.listen(4200, () => {
+    console.log('Started on port 4200')
+});
