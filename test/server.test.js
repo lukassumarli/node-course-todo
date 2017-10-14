7ec2e1e106f7e69f41378ba0136f7174d37ed966
Lukas Sumarli <lukassumarli@gmail.com>
2017-10-14 13:19:53 +0000

    Test script for GET /todos/:id

--- test/server.test.js
+++ test/server.test.js
@@ -1,17 +1,21 @@
 const request = require('supertest');
 const expect = require('expect');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server/server');
 const {Todo} = require('./../server/models/todo');
 
 const todos = [
     {
+        _id: new ObjectID(),
         text: 'First test todo'
     },
     {
+        _id: new ObjectID(),
         text: 'Second test todo'
     },
     {
+        _id: new ObjectID(),
         text: 'Third test todo'
     }
 ]
@@ -77,3 +81,29 @@ describe('GET /todos', () => {
             .end(done);
     });
 });
+
+describe('GET /todos/:id', ()=>{
+    it('should get the todo with :id id', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object id', (done) => {
+        request(app)
+            .get('/todos/123abc')
+            .expect(404)
+            .end(done);
+    })
+})
